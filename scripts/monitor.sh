1f08ea89ba05cac10ce78ea2f64710640dc2cd6a
Tomas Baca <klaxalk@gmail.com>
2023-05-29 14:52:16 +0200

    towards testing the flip

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -12,7 +12,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 UAV_NAME=$1
 PROJECT_NAME=monitor_$UAV_NAME
 
-~/.i3/detacher.sh 1 "~/.scripts/set_ros_master_uri.sh $UAV_NAME"
+# ~/.i3/detacher.sh 1 "~/.scripts/set_ros_master_uri.sh $UAV_NAME"
 
 # following commands will be executed first, in each window
 pre_input="export UAV_NAME=$UAV_NAME"
